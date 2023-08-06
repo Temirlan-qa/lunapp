2d8b9304b937bd099b384f7a4d3d5d47496c1259
Temirlan-qa <temirlanaazbek@gmail.com>
2023-08-06 08:31:14 +0600

    init repo

--- lib/src/features/presentation/home/page/home_screen.dart
+++ lib/src/features/presentation/home/page/home_screen.dart
@@ -6,26 +6,40 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        color: Color(0xFF9B9B9B),
-        padding: EdgeInsets.symmetric(horizontal: 32.w),
-        child: Column(
-          children: [
-            Container(
-              color: Colors.white,
-              height: 50.h,
-              width: MediaQuery.of(context).size.width - 64.w,
-            ),
-            SizedBox(height: 26.h),
-            TextButton(
-              style: TextButton.styleFrom(
-                textStyle: TextStyle(fontSize: 16.sp),
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          color: Color(0xFF9B9B9B),
+          padding: EdgeInsets.symmetric(horizontal: 32.w),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                color: Colors.white,
+                height: 50.h,
+                width: MediaQuery.of(context).size.width - 64.w,
               ),
-              onPressed: null,
-              child: const Text('Начать урок'),
-            ),
-          ],
+              SizedBox(height: 26.h),
+              TextButton(
+                style: TextButton.styleFrom(
+                  textStyle: TextStyle(
+                    fontSize: 16.sp,
+                    color: Colors.white,
+                  ),
+                  primary: Colors.white,
+                  backgroundColor: Color(0xFF433E3E),
+                ),
+                onPressed: null,
+                child: Text(
+                  'Начать урок',
+                  style: TextStyle(
+                    fontSize: 16.sp,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
